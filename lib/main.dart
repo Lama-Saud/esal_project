10f1e21b7c1860502dc1d72727f08cc062e790a1
Lama Saud <lama.saud.s@gmail.com>
2023-03-04 18:43:23 +0300

    updated login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,41 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:final_project/pages/invoices/add_inv_page.dart';
+// import 'package:animated_splash_screen/animated_splash_screen.dart';
+// import 'package:final_project/pages/invoices/add_inv_page.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       home: AnimatedSplashScreen(
+//         splash: Icons.home,
+//         duration: 3000,
+//         splashTransition: SplashTransition.fadeTransition,
+//         nextScreen: const AddInvPage(),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'pages/login/login_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MyApp());
 }
 
@@ -11,15 +44,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: AnimatedSplashScreen(
-        splash: Icons.home,
-        duration: 3000,
-        splashTransition: SplashTransition.fadeTransition,
-        nextScreen: const AddInvPage(),
-      ),
+      title: 'TheGorgeousLogin',
+      home: LoginPage(),
     );
   }
 }
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,84 +1,246 @@
-import 'package:final_project/pages/login/signup_page.dart';
+// import 'package:final_project/pages/login/signup_page.dart';
+// import 'package:flutter/material.dart';
+
+// import 'components/esal_button.dart';
+// import 'components/esal_text_field.dart';
+
+// class LoginPage extends StatelessWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color.fromARGB(255, 80, 180, 226),
+//       body: Container(
+//         margin: const EdgeInsets.only(top: 80),
+//         decoration: const BoxDecoration(
+//             borderRadius: BorderRadius.only(
+//               topLeft: Radius.circular(30),
+//               topRight: Radius.circular(30),
+//             ),
+//             color: Colors.white),
+//         child: Padding(
+//           padding: const EdgeInsets.all(40.0),
+//           child: SizedBox(
+//             width: double.infinity,
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               crossAxisAlignment: CrossAxisAlignment.end,
+//               children: [
+//                 Image.asset('lib/assets/esal_logo.png', width: 80),
+//                 const SizedBox(height: 30),
+//                 const Text('تسجيل الدخول', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+//                 const Text(
+//                   'إيصال هو تطبيق يساعدك على حفظ وتنظيم فواتيرك وضماناتك في مكان واحد',
+//                   style: TextStyle(fontSize: 16),
+//                   textAlign: TextAlign.right,
+//                 ),
+//                 const SizedBox(height: 40),
+//                 const EsalTextField(
+//                   title: 'البريد الإلكتروني',
+//                   icon: Icons.mail,
+//                 ),
+//                 const SizedBox(height: 10),
+//                 const EsalTextField(
+//                   icon: Icons.lock,
+//                   title: 'كلمة المرور',
+//                 ),
+//                 const SizedBox(height: 30),
+//                 const EsalButton(text: 'تسجيل دخول'),
+//                 Padding(
+//                   padding: const EdgeInsets.all(8),
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.end,
+//                     children: [
+//                       InkWell(
+//                         onTap: () => Navigator.of(context).push(
+//                           MaterialPageRoute(builder: (context) => const SignupPage()),
+//                         ),
+//                         child: const Text(
+//                           'تسجيل جديد',
+//                           textAlign: TextAlign.left,
+//                           style: TextStyle(
+//                             color: Color.fromARGB(255, 80, 180, 226),
+//                             fontWeight: FontWeight.bold,
+//                             fontSize: 15,
+//                           ),
+//                         ),
+//                       ),
+//                       const Text(
+//                         'لا يوجد لديك حساب؟ ',
+//                         textAlign: TextAlign.right,
+//                         style: TextStyle(fontSize: 15),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-import 'components/esal_button.dart';
-import 'components/esal_text_field.dart';
+// import 'package:the_gorgeous_login/pages/widgets/sign_in.dart';
+// import 'package:the_gorgeous_login/pages/widgets/sign_up.dart';
+// import 'package:the_gorgeous_login/theme.dart';
+// import 'package:the_gorgeous_login/utils/bubble_indicator_painter.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+import '../../theme.dart';
+import '../../utils/bubble_indicator_painter.dart';
+import 'widgets/sign_in.dart';
+import 'widgets/sign_up.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  late PageController _pageController;
+
+  Color left = Colors.black;
+  Color right = Colors.white;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 80, 180, 226),
-      body: Container(
-        margin: const EdgeInsets.only(top: 80),
-        decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-            color: Colors.white),
-        child: Padding(
-          padding: const EdgeInsets.all(40.0),
-          child: SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Image.asset('lib/assets/esal_logo.png', width: 80),
-                const SizedBox(height: 30),
-                const Text('تسجيل الدخول', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
-                const Text(
-                  'إيصال هو تطبيق يساعدك على حفظ وتنظيم فواتيرك وضماناتك في مكان واحد',
-                  style: TextStyle(fontSize: 16),
-                  textAlign: TextAlign.right,
+        body: SingleChildScrollView(
+      physics: const ClampingScrollPhysics(),
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: <Color>[CustomTheme.loginGradientStart, CustomTheme.loginGradientEnd],
+                begin: FractionalOffset(0.0, 0.0),
+                end: FractionalOffset(1.0, 1.0),
+                stops: <double>[0.0, 1.0],
+                tileMode: TileMode.clamp),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 75.0),
+                child: Image(
+                    height: MediaQuery.of(context).size.height > 800 ? 141.0 : 100,
+                    fit: BoxFit.fill,
+                    image: const AssetImage('lib/assets/esal_logo.png')),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: _buildMenuBar(context),
+              ),
+              Expanded(
+                flex: 2,
+                child: PageView(
+                  controller: _pageController,
+                  physics: const ClampingScrollPhysics(),
+                  onPageChanged: (int i) {
+                    FocusScope.of(context).requestFocus(FocusNode());
+                    if (i == 0) {
+                      setState(() {
+                        right = Colors.white;
+                        left = Colors.black;
+                      });
+                    } else if (i == 1) {
+                      setState(() {
+                        right = Colors.black;
+                        left = Colors.white;
+                      });
+                    }
+                  },
+                  children: <Widget>[
+                    ConstrainedBox(
+                      constraints: const BoxConstraints.expand(),
+                      child: const SignIn(),
+                    ),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints.expand(),
+                      child: const SignUp(),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 40),
-                const EsalTextField(
-                  title: 'البريد الإلكتروني',
-                  icon: Icons.mail,
-                ),
-                const SizedBox(height: 10),
-                const EsalTextField(
-                  icon: Icons.lock,
-                  title: 'كلمة المرور',
-                ),
-                const SizedBox(height: 30),
-                const EsalButton(text: 'تسجيل دخول'),
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      InkWell(
-                        onTap: () => Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => const SignupPage()),
-                        ),
-                        child: const Text(
-                          'تسجيل جديد',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 80, 180, 226),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ),
-                      const Text(
-                        'لا يوجد لديك حساب؟ ',
-                        textAlign: TextAlign.right,
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
+    ));
+  }
+
+  Widget _buildMenuBar(BuildContext context) {
+    return Container(
+      width: 300.0,
+      height: 50.0,
+      decoration: const BoxDecoration(
+        color: Color(0x552B2B2B),
+        borderRadius: BorderRadius.all(Radius.circular(25.0)),
+      ),
+      child: CustomPaint(
+        painter: BubbleIndicatorPainter(pageController: _pageController),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                ),
+                onPressed: _onSignInButtonPress,
+                child: Text(
+                  'Existing',
+                  style: TextStyle(color: left, fontSize: 16.0, fontFamily: 'WorkSansSemiBold'),
+                ),
+              ),
+            ),
+            //Container(height: 33.0, width: 1.0, color: Colors.white),
+            Expanded(
+              child: TextButton(
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                ),
+                onPressed: _onSignUpButtonPress,
+                child: Text(
+                  'New',
+                  style: TextStyle(color: right, fontSize: 16.0, fontFamily: 'WorkSansSemiBold'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _onSignInButtonPress() {
+    _pageController.animateToPage(0, duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
+  }
+
+  void _onSignUpButtonPress() {
+    _pageController.animateToPage(1, duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
   }
 }
